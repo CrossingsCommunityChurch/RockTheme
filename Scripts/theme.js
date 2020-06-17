02b19653fb5842f41cb244b1b80eb8e1bacc3d8d
Wes Gutekunst <wes@gutekunstdesign.com>
2020-06-17 12:02:13 -0500

    js scroll fixed nav

--- Scripts/theme.js
+++ Scripts/theme.js
@@ -1,1 +1,31 @@
 console.log('loading theme js');
+
+$(document).ready(function() {
+
+    $('.primary-nav').addClass('fixed');
+
+    $(".has-subnav").hover(function(){
+        $(this).find('.fas').toggleClass('fa-angle-up');
+    });
+
+    $('a[data-toggle="collapse"').click(function(){
+        $(this).find('.fa-angle-down').toggleClass('fa-angle-up');
+    });
+
+    $('.primary-locations h3 > a').click(function(){
+        $('#locationsModal').removeClass('in').hide();
+        $('.modal-backdrop').remove();
+        $('body').removeClass('modal-open');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 100) {
+            $('.primary-nav').addClass('fixed');
+        } else {
+            $('.primary-nav').removeClass('fixed');
+        }
+    });
+
+});
