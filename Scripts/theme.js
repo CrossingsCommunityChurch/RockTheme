5bd1b59c29c4463d0ef3d20cb7d628d2fd52aee9
Dillon Wilson <dillon@gutekunstdesign.com>
2022-06-14 14:59:42 -0500

    Removed push on desktop

--- Scripts/theme.js
+++ Scripts/theme.js
@@ -9,64 +9,69 @@ $(document).ready(function() {
         duration: '1000'
     });
 
-    $('.card-list-filter .btn').click(function(e){
+    $('.card-list-filter .btn').click(function(e) {
         e.preventDefault();
         $('.card-list-filter .btn').removeClass('active');
         $(this).toggleClass('active');
         $('.media-list').toggleClass('list');
     });
 
-    $('.has-subnav-parent').hover(function(){
+    $('.has-subnav-parent').hover(function() {
         $(this).find('.fa-angle-down').toggleClass('fa-angle-up');
     });
 
-    $('a[data-toggle="collapse"]').click(function(){
+    $('a[data-toggle="collapse"]').click(function() {
         $(this).find('.fa-angle-down').toggleClass('fa-angle-up');
     });
 
-    $('.dropdown-toggle').click(function(){
+    $('.dropdown-toggle').click(function() {
         $(this).find('.fa-caret-down').toggleClass('fa-caret-up');
     });
 
-    $('.primary-locations h3 > a').click(function(){
+    $('.primary-locations h3 > a').click(function() {
         $('#locationsModal').removeClass('in').hide();
         $('.modal-backdrop').remove();
         $('body').removeClass('modal-open');
     });
 
-    $('.mobile-slideout-close').click(function(e){
+    $('.mobile-slideout-close').click(function(e) {
         e.preventDefault();
         $('.mobile-nav-bar-item').removeClass('active');
         $('.mobile-slideout, .mobile-overlay').removeClass('active');
     })
 
-    $('.toggle-locations').click(function(e){
+    $('.toggle-locations').click(function(e) {
         e.preventDefault();
         $('.mobile-nav-bar-item').removeClass('active');
         $(this).add('.mobile-slideout, .mobile-overlay').removeClass('active');
         $(this).add('.mobile-slideout.locations, .mobile-overlay').addClass('active');
     });
 
-    $('.toggle-media').click(function(e){
+    $('.toggle-media').click(function(e) {
         e.preventDefault();
         $('.mobile-nav-bar-item').removeClass('active');
         $(this).add('.mobile-slideout, .mobile-overlay').removeClass('active');
         $(this).add('.mobile-slideout.media, .mobile-overlay').addClass('active');
     });
 
-    $(window).scroll(function() {    
+    $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
         if (scroll >= 140) {
-            $('.primary-nav').addClass('fixed push');
+            $('.primary-nav').addClass('fixed');
         } else {
-            $('.primary-nav').removeClass('fixed push sticky');
+            $('.primary-nav').removeClass('fixed sticky');
             // $('.primary-nav').removeAttr('style');
+        }
+
+        let windowSize = $(window).width();
+        if (windowSize < 992) {
+            $('.primary-nav').addClass('push');
         }
     });
 
     let menuWidth = '30em';
-    let windowSize = $( window ).width();
+    let windowSize = $(window).width();
 
     if (windowSize < 992) {
         menuWidth = '75%';
@@ -86,7 +91,7 @@ $(document).ready(function() {
     $('body').addClass('zoom');
 
 
-    $('.open-search').click(function(e){
+    $('.open-search').click(function(e) {
         e.preventDefault();
         $('.search-input').focus();
         $('.search-wrapper').addClass('open');
@@ -94,7 +99,7 @@ $(document).ready(function() {
         $('html').addClass('hide-overflow');
     });
 
-    $('.search-close').click(function(e){
+    $('.search-close').click(function(e) {
         e.preventDefault();
         $('.search-wrapper').removeClass('open');
         $('.mobile-navbar').removeClass('close');
@@ -107,7 +112,7 @@ $(document).ready(function() {
         window.location.href = '/search/?Q=' + input;
     });
 
-    $('.search-wrapper .search-input').keydown(function(event){
+    $('.search-wrapper .search-input').keydown(function(event) {
         if (event.key === 'Enter') {
             console.log('enter pressed')
             event.preventDefault();
@@ -115,7 +120,7 @@ $(document).ready(function() {
         }
     })
 
-    $(document).on('click', '[data-toggle="tab-cc"]', function (e) {
+    $(document).on('click', '[data-toggle="tab-cc"]', function(e) {
         e.preventDefault();
         console.log('wes');
         $(this).closest('.nav-tabs').find('li').removeClass('active');
@@ -129,8 +134,8 @@ $(document).ready(function() {
     //Microsite Stuff
     var offset = $("#zone-pageheading").outerHeight(true) - 140;
     console.log(offset);
-    $(".navbar.affix-navbar").affix({offset: {top: offset} });
-    
+    $(".navbar.affix-navbar").affix({ offset: { top: offset } });
+
     function isIE() {
         // IE 10 and IE 11
         return /Trident\/|MSIE/.test(window.navigator.userAgent);
@@ -138,16 +143,16 @@ $(document).ready(function() {
 
     function showBrowserAlert() {
         if (document.querySelector('.unsupported-browser')) {
-           var d = document.getElementsByClassName('unsupported-browser');
-    
-           var is_IE = isIE();
-    
+            var d = document.getElementsByClassName('unsupported-browser');
+
+            var is_IE = isIE();
+
             if (is_IE) {
                 d[0].innerHTML = '<div class="alert alert-info text-center" style="margin-bottom: 0;"><strong>Unsupported Browser</strong> This website will offer limited functionality in this browser. We only support the recent versions of major browsers like Chrome, Firefox, Safari, and Edge.</div>';
                 d[0].style.display = 'block';
             }
         }
     };
-    
+
     showBrowserAlert();
 });
