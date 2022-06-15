b60f956dcb58a74b3734e84ace75d4a737351268
Dillon Wilson <dillon@gutekunstdesign.com>
2022-06-15 10:44:31 -0500

    Another try

--- Scripts/theme.js
+++ Scripts/theme.js
@@ -84,14 +84,15 @@ $(document).ready(function() {
         // $('.primary-nav').css('left', '30em');
     });
 
-    $('#close-left-arrow').bigSlide.click({
-        state: closed
-    }).click(
-        function(e) {
-            e.preventDefault();
-            $('#account-menu').css('left', '-30em');
-            $('#login').removeClass('active');
-        });
+    $('#close-left-arrow').bigSlide({
+        menu: '#account-menu',
+        menuWidth: menuWidth,
+        easyClose: false,
+    }).click(function(e) {
+        e.preventDefault();
+        $('#account-menu').css('left', '-30em');
+        $('#login').removeClass('active');
+    });
 
     $('body').addClass('zoom');
 
