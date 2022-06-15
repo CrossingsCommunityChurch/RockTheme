20c1a838f388e1184a9730ccf5cb83fb8d62422e
Dillon Wilson <dillon@gutekunstdesign.com>
2022-06-15 11:29:54 -0500

    Added bigslide to #close-left-arrow

--- Scripts/theme.js
+++ Scripts/theme.js
@@ -83,10 +83,20 @@ $(document).ready(function() {
         // $('.primary-nav').css('left', '30em');
     });
 
-    $('#close-left-arrow').click(function(e) {
+    // $('#close-left-arrow').click(function(e) {
+    //     e.preventDefault();
+    //     $('#account-menu').css('left', '-30em');
+    //     $('#login').removeClass('active');
+    // });
+
+    $('#close-left-arrow').bigSlide({
+        menu: '#account-menu',
+        menuWidth: menuWidth,
+        easyClose: true,
+    }).click(function(e) {
         e.preventDefault();
         $('#account-menu').css('left', '-30em');
-        $('#login').removeClass('active');
+        // $('#login').removeClass('active');
     });
 
     $('body').addClass('zoom');
