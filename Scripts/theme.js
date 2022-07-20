592f58cbdc5e2f391913705977a33da82f320200
Dillon Wilson <dillon@gutekunstdesign.com>
2022-07-20 09:37:28 -0500

    Another try

--- Scripts/theme.js
+++ Scripts/theme.js
@@ -22,6 +22,9 @@ $(document).ready(function() {
 
     $('a[data-parent="#accordion"]').click(function() {
         $(this).find('.fa-angle-down').toggleClass('fa-angle-up');
+        if (!$(this).hasClass('collapsed')) {
+            $('a[data-parent="#accordion"]').find('fa-angle-up').toggleClass('fa-angle-up');
+        }
     });
 
     $('.dropdown-toggle').click(function() {
